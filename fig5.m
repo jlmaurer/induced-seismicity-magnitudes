bdc7b0ce4c72bebc5b2636f33bbc0216ae8206c5
Jeremy Maurer <jlmaurer@stanford.edu>
2018-01-18 21:41:54 -0800

    Script for fig. 5

---
+++ fig5.m
@@ -0,0 +1,36 @@
+%% Script to generate figures from Maurer & Segall, Fig. 5
+
+% pick a(t), rho_max
+a = 2500;           % 250 or 2500 m
+maxrho = 1500;      % 500 or 1500 m
+
+% other parameters
+minrho = mw2rs(0.5); 
+b = 1;
+Nx = 1000; 
+
+% Compute the analytical expression
+[ Prs, rx, Frs,Pin, Pp, Pr] = Compute_Prs_num (maxrho/a,minrho/a, b,Nx);
+
+% Plot the individual contribution
+figure; 
+loglog(rx.*a, Pin./a, 'b')
+hold on
+loglog(rx.*a, Pp./a, '--')
+loglog(rx.*a, Pr./a, ':', 'LineWidth', 2)
+plot([a, a], [min([Pp(:); Pin(:)]./a), max([Pp(:); Pin(:)]./a)])
+xlabel('Normalized Fault Size')
+ylabel('Relative Frequency')
+legend('P_{in}', 'P_p', 'P_r', 'a(t)')
+title('Components of P(r_s)')
+
+% Plot the full solution and compare to Segall & Lu, 2015
+figure; 
+loglog(rx.*a, Prs./a)
+hold on
+loglog(rx.*a, Pin./a, '--')
+plot([a, a], [min([Prs(:); Pin(:)]./a), max([Prs(:); Pin(:)]./a)])
+xlabel('M_W')
+ylabel('Relative Frequency')
+legend('Maurer-Segall 2017', 'Segall-Lu 2015', 'a(t)')
+title('Comparing Maurer & Segall 2017 to Segall & Lu 2015')

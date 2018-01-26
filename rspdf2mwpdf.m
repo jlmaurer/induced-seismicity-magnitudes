db32020d826172eaaf86020406258958af7b0f8e
Jeremy Maurer <jlmaurer@stanford.edu>
2018-01-26 13:00:31 -0800

    Correct call to rs2mw

--- rspdf2mwpdf.m
+++ rspdf2mwpdf.m
@@ -2,7 +2,7 @@ function [mws, fMw] = rspdf2mwpdf(rs, frs, dtau, c)
 % this function converts from the function on rs to the function on
 % magnitude
 
-    mws = rs2mw(rs, dtau);  
+    mws = rs2mw(rs, dtau, c);  
     ex = .5*(mws(:) + 6.03); 
     numer = log(10)/2; 
     denom = (pi*dtau/c)^(1/3);

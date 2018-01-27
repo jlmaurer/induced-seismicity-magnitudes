1d5bda40a96a93f7e10539fdbef0104c815dc5ed
Jeremy Maurer <jlmaurer@stanford.edu>
2018-01-27 11:10:28 -0800

    Clean up

--- Compute_Pin.m
+++ Compute_Pin.m
@@ -1,11 +1,17 @@
-function [Pin] = Compute_Pin(rx, b, rmin, a)
+function [Pin] = Compute_Pin(rx, a)
 % This function computes Pin, as presented by Maurer & Segall (2018), and
-% originally formulated in Segall & Lu (2015). 
+% originally formulated in Segall & Lu (2015). Does NOT include the GRD
+% term, so this term must be multiplied by GRD to get the full
+% distribution. 
 
-    rxda = rx./a; 
-    Pgrd = computeGRD(rxda, b, rmin); 
+    if nargin < 2
+        rxda = rx; 
+    else
+        rxda = rx./a; 
+    end
+    
     zc = sqrt(1 - rxda.^2); 
     racr = rxda.*acos(rxda); 
     
-    Pin = 0.5*(zc - racr - ((zc.^3)/3)).*Pgrd; 
+    Pin = 0.5*(zc - racr - ((zc.^3)/3)); 
 end

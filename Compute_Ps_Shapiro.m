2aca764514001d9b3c9aada10211c0d851943ceb
Jeremy Maurer <jlmaurer@stanford.edu>
2018-01-26 16:55:28 -0800

    Add fcn for Shapiro models

---
+++ Compute_Ps_Shapiro.m
@@ -0,0 +1,27 @@
+function [Psl, Psc, Psu] = compute_Ps_Shapiro (rx)
+% This function returns the geometrical modifying factors from Shapiro et
+% al. (2011, 2013, 2015), by default assuming a uniform distribution on
+% fault size rho. Can be multiplied by GRD to get the corresponding
+% distribution assuming GRD. 
+
+    if max(rx) <=1
+        zc = sqrt(1 - (rx.^2)); 
+    else
+        error('Need to normalize r_s by a(t)')
+    end
+
+    % first: P_s/P_c = P_vol
+    asr = asin(rx); 
+    t1 = 1 + .5*(rx.^2); 
+    t2 = zc; 
+    t3 = 3*pi*rx/4; 
+    t4 = 1.5*rx.*asr; 
+    Psl = t1.*t2 - t3 + t4; 
+
+    % second, P_s/P_c = 1
+    Psc = ones(size(Psl)); 
+
+    % third, P_s/P_c = 1/P_c
+    Psu = 1 + 3*(rx.^2)/2 + (3*pi*rx/4); 
+
+end
